274762d88d9725a9e855129f8846adb420dc9466
Patricio Monpelat <patricio.monpelat@gmail.com>
2021-11-09 12:07:47 -0300

    js files

---
+++ assets/js/extra.js
@@ -0,0 +1,14 @@
+
+function resetForm(){
+	var inputName = document.getElementById('name');
+	var inputEmail = document.getElementById('email');
+	var inputMessage = document.getElementById('message');
+	
+	inputName.value="";
+	inputEmail.value="";
+	inputMessage.value="";
+}
+
+function validateform(){
+
+}
