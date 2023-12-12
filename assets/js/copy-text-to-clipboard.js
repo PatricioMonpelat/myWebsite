4b77eb2c2c393cb74b525d490ff352017b98e7ee
Patricio Monpelat <patricio.monpelat@gmail.com>
2023-12-12 14:30:50 -0300

    .

--- assets/js/copy-text-to-clipboard.js
+++ assets/js/copy-text-to-clipboard.js
@@ -1,14 +1,14 @@
 function myFunction() {
-    // Get the text field
-    var copyText = document.getElementById("myInput");
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
   
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-  
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
-  }
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
+}
