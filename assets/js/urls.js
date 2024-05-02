38840494cb570aca1ca11545df2d5cb5c0f9ae22
Patricio Monpelat <patricio.monpelat@gmail.com>
2024-05-02 18:07:14 -0300

    .

---
+++ assets/js/urls.js
@@ -0,0 +1,36 @@
+var urls = {
+  portfolio: {
+    1: {
+      liveDemo: "https://master.d2lno1hp5sufbz.amplifyapp.com/",
+      sourceCode: "https://github.com/PatricioMonpelat/MemeAr.git",
+    },
+    2: {
+      liveDemo: "",
+      sourceCode: "",
+    },
+    3: {
+      liveDemo: "",
+      sourceCode: "",
+    },
+    4: {
+      liveDemo: "",
+      sourceCode: "",
+    },
+    5: {
+      liveDemo: "",
+      sourceCode: "",
+    },
+    6: {
+      liveDemo: "",
+      sourceCode: "",
+    },
+    7: {
+      liveDemo: "",
+      sourceCode: "",
+    },
+    8: {
+      liveDemo: "",
+      sourceCode: "",
+    },
+  },
+};
