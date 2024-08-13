11ce3b2c3f7766e2e68fdd8502f6c36084376f7a
Patricio Monpelat <patricio.monpelat@gmail.com>
2024-08-13 18:02:42 -0300

    .

--- assets/js/urls.js
+++ assets/js/urls.js
@@ -1,7 +1,6 @@
 var urls = {
   portfolio: {
     1: {
-<<<<<<< HEAD
       liveDemo: "https://netflix-clone-80fc2.web.app/",
       sourceCode: "https://github.com/PatricioMonpelat/netflix-clone.git",
       project1_mock: "https://monpelat.dev/images/projects/project1_mock.webp",
@@ -9,7 +8,7 @@ var urls = {
     2: {
       liveDemo: "https://main.d12tjqlp7iebde.amplifyapp.com/",
       sourceCode: "https://github.com/PatricioMonpelat/Angular-RecipeBook.git",
-      project2_mock:"https://monpelat.dev/images/projects/project2_mock.webp",
+      project2_mock: "https://monpelat.dev/images/projects/project2_mock.webp",
     },
     3: {
       liveDemo: "https://master.d2wdqoywqdq366.amplifyapp.com/",
@@ -17,14 +16,10 @@ var urls = {
       project3_mock: "https://monpelat.dev/images/projects/project3_mock.webp",
     },
     4: {
-=======
->>>>>>> parent of 70e8843 (.)
       liveDemo: "https://master.d2lno1hp5sufbz.amplifyapp.com/",
       sourceCode: "https://github.com/PatricioMonpelat/MemeAr.git",
       project4_mock: "https://monpelat.dev/images/projects/project4_mock.webp",
     },
-<<<<<<< HEAD
-=======
     2: {
       liveDemo: "",
       sourceCode: "",
@@ -34,15 +29,14 @@ var urls = {
       sourceCode: "",
     },
     4: {
-        liveDemo: "https://master.d2lno1hp5sufbz.amplifyapp.com/",
-        sourceCode: "https://github.com/PatricioMonpelat/MemeAr.git",
-        project4_mock: "https://monpelat.dev/images/projects/project4_mock.webp",
+      liveDemo: "https://master.d2lno1hp5sufbz.amplifyapp.com/",
+      sourceCode: "https://github.com/PatricioMonpelat/MemeAr.git",
+      project4_mock: "https://monpelat.dev/images/projects/project4_mock.webp",
     },
     5: {
       liveDemo: "",
       sourceCode: "",
     },
->>>>>>> parent of 70e8843 (.)
     6: {
       liveDemo: "",
       sourceCode: "",
@@ -57,7 +51,6 @@ var urls = {
     },
   },
   skills: {
-<<<<<<< HEAD
     angular: "https://monpelat.dev/images/skills/angular.webp",
     aws: "https://monpelat.dev/images/svg/aws.svg",
     react: "https://monpelat.dev/images/skills/react.webp",
@@ -71,16 +64,12 @@ var urls = {
     linkedin: "https://www.linkedin.com/in/patricio-monpelat-872172133/",
     github: "https://github.com/PatricioMonpelat",
     gmail: "mailto:patricio.monpelat@gmail.com",
-=======
-    angular: 'https://monpelat.dev/images/skills/angular.webp',
-    aws:'https://monpelat.dev/images/svg/aws.svg',
-
->>>>>>> parent of 70e8843 (.)
+    angular: "https://monpelat.dev/images/skills/angular.webp",
+    aws: "https://monpelat.dev/images/svg/aws.svg",
   },
-  personal:{
-    linkedin: 'https://www.linkedin.com/in/patricio-monpelat-872172133/',
-    github:'https://github.com/PatricioMonpelat',
-    gmail: 'mailto:patricio.monpelat@gmail.com'
-  }
-
+  personal: {
+    linkedin: "https://www.linkedin.com/in/patricio-monpelat-872172133/",
+    github: "https://github.com/PatricioMonpelat",
+    gmail: "mailto:patricio.monpelat@gmail.com",
+  },
 };
