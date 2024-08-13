9c25dbcef6f0d8fbc5b26352f3c45a2813a8d967
Patricio Monpelat <patricio.monpelat@gmail.com>
2024-08-13 18:23:26 -0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@
    document.getElementById('firebaseSkill').src = urls.skills.firebase;
   // document.getElementById('nextSkill').src = urls.skills.next;
   // document.getElementById('tailwindSkill').src = urls.skills.tailwind;
-   document.getElementById('vercelSkill').src = urls.skills.vercel;
+//    document.getElementById('vercelSkill').src = urls.skills.vercel;
    document.getElementById('stripeSkill').src = urls.skills.stripe;
 
 
