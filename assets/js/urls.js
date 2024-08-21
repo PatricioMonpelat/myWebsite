079c1741563ad40af36d2a7852bb90e8e6b173bc
Patricio Monpelat <patricio.monpelat@gmail.com>
2024-08-21 15:01:12 -0300

    .

--- assets/js/urls.js
+++ assets/js/urls.js
@@ -3,69 +3,35 @@ var urls = {
     1: {
       liveDemo: "https://netflix-clone-80fc2.web.app/",
       sourceCode: "https://github.com/PatricioMonpelat/netflix-clone.git",
-      project1_mock: "https://monpelat.dev/images/projects/project1_mock.webp",
     },
     2: {
       liveDemo: "https://main.d12tjqlp7iebde.amplifyapp.com/",
       sourceCode: "https://github.com/PatricioMonpelat/Angular-RecipeBook.git",
-      project2_mock: "https://monpelat.dev/images/projects/project2_mock.webp",
     },
     3: {
       liveDemo: "https://master.d2wdqoywqdq366.amplifyapp.com/",
       sourceCode: "https://github.com/PatricioMonpelat/react-nft.git",
-      project3_mock: "https://monpelat.dev/images/projects/project3_mock.webp",
     },
     4: {
       liveDemo: "https://master.d2lno1hp5sufbz.amplifyapp.com/",
       sourceCode: "https://github.com/PatricioMonpelat/MemeAr.git",
-      project4_mock: "https://monpelat.dev/images/projects/project4_mock.webp",
-    },
-    2: {
-      liveDemo: "",
-      sourceCode: "",
-    },
-    3: {
-      liveDemo: "",
-      sourceCode: "",
-    },
-    4: {
-      liveDemo: "https://master.d2lno1hp5sufbz.amplifyapp.com/",
-      sourceCode: "https://github.com/PatricioMonpelat/MemeAr.git",
-      project4_mock: "https://monpelat.dev/images/projects/project4_mock.webp",
     },
     5: {
       liveDemo: "",
       sourceCode: "",
     },
     6: {
-      liveDemo: "",
-      sourceCode: "",
+      liveDemo: "https://todo-app-d28bb.web.app/",
+      sourceCode: "https://github.com/PatricioMonpelat/todo-app.git",
     },
     7: {
-      liveDemo: "",
-      sourceCode: "",
+      liveDemo: "https://airbnb-clone-rouge-nine.vercel.app/",
+      sourceCode: "https://github.com/PatricioMonpelat/airbnb-clone",
     },
     8: {
-      liveDemo: "",
-      sourceCode: "",
+      liveDemo: "https://portfolio-template-patriciomonpelat.vercel.app/",
+      sourceCode: "https://github.com/PatricioMonpelat/portfolio-template",
     },
-  },
-  skills: {
-    angular: "https://monpelat.dev/images/skills/angular.webp",
-    aws: "https://monpelat.dev/images/svg/aws.svg",
-    react: "https://monpelat.dev/images/skills/react.webp",
-    firebase: "https://monpelat.dev/images/skills/firebase.webp",
-    next: "https://monpelat.dev/images/skills/next.webp",
-    tailwind: "https://monpelat.dev/images/skills/tailwind.webp",
-    vercel: "https://monpelat.dev/images/skills/vercel.webp",
-    stripe: "https://monpelat.dev/images/skills/stripe.webp",
-  },
-  personal: {
-    linkedin: "https://www.linkedin.com/in/patricio-monpelat-872172133/",
-    github: "https://github.com/PatricioMonpelat",
-    gmail: "mailto:patricio.monpelat@gmail.com",
-    angular: "https://monpelat.dev/images/skills/angular.webp",
-    aws: "https://monpelat.dev/images/svg/aws.svg",
   },
   personal: {
     linkedin: "https://www.linkedin.com/in/patricio-monpelat-872172133/",
